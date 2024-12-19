ef006ce83f7658ec815ed80a162b1b422550e584
diana5hodzic <diana5hodzic@gmail.com>
2024-12-19 02:32:24 +0100

    update diskusija

--- client/src/components/Discussions.js
+++ client/src/components/Discussions.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { fetchAllDiscussions } from "../services/discussionService";
+import { fetchThemes} from '../services/themeService';
+import { addDiscussion } from '../services/themeService';
 import DiscussionDisplay from './DiscussionDisplay'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 // Ovdje se samo filtriraju sve diskusije!
 
 const Discussions = ({ userId }) => {
+
+  // dodavanje diskusije
+  const [themes, setThemes] = useState([]); // Lista tema
+  const [selectedFromList, setSelectedFromList] = useState(null); 
+  const [newDiscussionText, setNewDiscussionText] = useState(''); 
+  const [isAddModalOpen, setAddModalOpen] = useState(false); 
+
+  // prikaz i filtriranje diskusija
   const [discussions, setDiscussions] = useState([]);
   const [filters, setFilters] = useState({
     searchBy: 'theme_name',
@@ -52,54 +63,161 @@ const Discussions = ({ userId }) => {
     return matchesSearch;
   });
 
-  return (
-    <div className="container mt-4">
-      <h2 className="text-center mb-4">Diskusije</h2>
+ //***ZA DODAVANJE DISKUSIJE */
+  const openAddModal = () => {
+    setAddModalOpen(true);
+  };
 
-      <div className="d-flex justify-content-center mb-4">
-        <div className="form-group mx-2">
-          <div className="btn-group" role="group" aria-label="Search by">
-            {['theme_name', 'name', 'surname', 'email'].map((type) => (
-              <label key={type} className="btn btn-outline-primary">
-                <input
-                  type="radio"
-                  name="searchBy"
-                  value={type}
-                  checked={filters.searchBy === type}
-                  onChange={handleRadioChange}
-                />
-                {type.charAt(0).toUpperCase() + type.slice(1).replace('_', ' ')}
-              </label>
-            ))}
+  const closeAddModal = () => {
+    setAddModalOpen(false);
+    setNewDiscussionText('');
+    setSelectedFromList(null);
+  };
+
+  const handleAddDiscussion = () => {
+    if (!selectedFromList) {
+      alert('Please select a theme.');
+      return;
+    }
+
+    if (newDiscussionText.trim() === '') {
+      alert('Discussion text cannot be empty.');
+      return;
+    }
+
+    addDiscussion(selectedFromList, newDiscussionText)
+      .then(() => {
+        alert('Discussion added successfully.');
+        closeAddModal();
+      })
+      .catch((error) => {
+        console.error('Error adding discussion:', error);
+        alert('Failed to add discussion.');
+      });
+  };
+
+  useEffect(() => {
+    fetchThemes()
+      .then((response) => {
+        const sortedThemes = response.data.sort(
+          (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
+        );
+        setThemes(sortedThemes);
+      })
+      .catch((error) => console.error('Error fetching themes:', error));
+
+    /* Ovde možemo dodati socket logiku, ako je potrebno
+    return () => {
+      socket.off("serverReaction");
+      socket.off("mention_notification");
+    };*/ 
+    // u uglaste zagrade socket i proslije kao prop onda!
+  }, []);
+
+  return (
+  <div className="container mt-4">
+    <h2 className="text-center mb-4">Diskusije</h2>
+
+    {/* Add Discussion Modal */}
+    {isAddModalOpen && (
+      <div className="edit-modal">
+        <div className="modal-content">
+          <h4>Add Discussion</h4>
+          <form>
+            <label htmlFor="themeDropdown">Select Theme:</label>
+            <select
+              id="themeDropdown"
+              className="form-select"
+              value={selectedFromList ? selectedFromList.id : ''}
+              onChange={(e) =>
+                setSelectedFromList(
+                  themes.find((theme) => theme.id === parseInt(e.target.value))
+                )
+              }>
+              <option value="" disabled>
+                Choose a theme...
+              </option>
+              {themes.map((theme) => (
+                <option key={theme.id} value={theme.id}>
+                  {theme.theme_name}
+                </option>
+              ))}
+            </select>
+
+            <label htmlFor="discussionText" className="mt-3">
+              Discussion Text:
+            </label>
+            <textarea
+              id="discussionText"
+              className="form-control"
+              rows="5"
+              value={newDiscussionText}
+              onChange={(e) => setNewDiscussionText(e.target.value)}
+              placeholder="Enter your discussion here..."
+            ></textarea>
+          </form>
+
+          <div className="mt-3">
+            <button className="btn btn-info me-2" onClick={handleAddDiscussion}>
+              Add
+            </button>
+            <button className="btn btn-secondary" onClick={closeAddModal}>
+              Close
+            </button>
           </div>
         </div>
+      </div>
+    )}
 
-        <div className="form-group mx-2" style={{ maxWidth: '300px' }}>
-          <input
-            type="text"
-            name="searchValue"
-            value={filters.searchValue}
-            onChange={handleFilterChange}
-            className="form-control"
-            placeholder="Unesite tekst za pretragu..."
-          />
-        </div>
+    <div className="d-flex justify-content-center mb-4 align-items-center">
+      <div className="btn-group" role="group" aria-label="Search by">
+        {['theme_name', 'name', 'surname', 'email'].map((type) => (
+          <label key={type} className="btn btn-outline-primary">
+            <input
+              type="radio"
+              name="searchBy"
+              value={type}
+              checked={filters.searchBy === type}
+              onChange={handleRadioChange}
+            />
+            {type.charAt(0).toUpperCase() + type.slice(1).replace('_', ' ')}
+          </label>
+        ))}
       </div>
 
-      <div className="d-flex flex-column align-items-center">
-        {filteredDiscussions.length > 0 ? (
-          filteredDiscussions.map((discussion) => (
-            <DiscussionDisplay 
-            key={discussion.id} 
-            discussion={discussion} 
-            userId={userId}
-          />
-          ))
-        ) : (
-          <p>No discussions found matching your search criteria.</p>
-        )}
+      <div className="mx-2" style={{ maxWidth: '300px' }}>
+        <input
+          type="text"
+          name="searchValue"
+          value={filters.searchValue}
+          onChange={handleFilterChange}
+          className="form-control"
+          placeholder="Unesite tekst za pretragu..."
+        />
+      </div>
+
+      <div>
+        <button
+          className="btn btn-outline-secondary"
+          onClick={openAddModal}
+          style={{ margin: '1rem' }}>
+          <i className="fa fa-plus" style={{ marginRight: '0.5rem' }}></i> {/* Plus ikona */}
+          Create Post
+        </button>
       </div>
     </div>
+
+    <div className="d-flex flex-column align-items-center">
+      {filteredDiscussions.length > 0 ? (
+        filteredDiscussions.map((discussion) => (
+          <DiscussionDisplay key={discussion.id} discussion={discussion} userId={userId} />
+        ))
+      ) : (
+        <p>No discussions found matching your search criteria.</p>
+      )}
+    </div>
+  </div>
+
   );
 };
 
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,44 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/user.css';
-import { fetchThemes, fetchDiscussions, fetchDiscussionsOfUser } from '../services/themeService';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
 import { getUserByUsername, updateUser } from '../services/authService';
-import { addDiscussion } from '../services/themeService';
-import { faPen } from '@fortawesome/free-solid-svg-icons';
-import { getDiscussionById, updateDiscussion, deleteDiscussion } from '../services/themeService';
-import { faList } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons'; // Import the delete icon
 import Discussions from "./Discussions";
-
-//treba ovo
-import { getUserIdByUsername } from '../services/discussionService';
+import { getUserIdByUsername } from '../services/discussionService'; // ovo treba
 
 const User = ({ socket, handleLogout }) => {
   //TREBA ZA PROP - kod DISKUSIJA
   const [userId, setUserId] = useState(null);
 
-  //Teme
-  const [themes, setThemes] = useState([]); // Lista tema
-  const [filteredThemes, setFilteredThemes] = useState([]); // Filtrirane teme
-  const [searchTerm, setSearchTerm] = useState(''); // Unos u polje za pretragu
-  const [selectedTheme, setSelectedTheme] = useState(null); // Selektovana tema
-
-  const [discussionText, setDiscussionText] = useState(''); // Tekst diskusije
-  const [discussions, setDiscussions] = useState([]); // Diskusije za selektovanu temu
+  // USER
   const [editUser, setEditUser] = useState(null); // Podaci o korisniku za uređivanje
   const [isEditModalOpen, setEditModalOpen] = useState(false); // Modal za uređivanje
-  const [editDiscussion, setEditDiscussion] = useState(null);
-  const [isEditDiscussionModalOpen, setEditDiscussionModalOpen] = useState(false); // Modal za uređivanje
-  const [userDiscussions, setUserDiscussions] = useState([]); // Discussions of the logged-in user
-
-  const [isUserDiscussionsVisible, setUserDiscussionsVisible] = useState(false); // New state
-  const [isDiscussionsVisible, setDiscussionsVisible] = useState(false); // New state
-
-  // za dodavanje diskusije
-  const [selectedFromList, setSelectedFromList] = useState(null); // Selected theme for new discussion
-  const [newDiscussionText, setNewDiscussionText] = useState(''); // New discussion text
-  const [isAddModalOpen, setAddModalOpen] = useState(false); // Modal for adding a new discussion
 
   //treba za PROP ZA diskusije
   useEffect(() => {
@@ -53,152 +25,7 @@ const User = ({ socket, handleLogout }) => {
       }
     }, []);
 
-  //***ZA DODAVANJE DISKUSIJE */
-
-  // Open the "Add Discussion" modal
-  const openAddModal = () => {
-    setAddModalOpen(true);
-  };
-
-  // Close the "Add Discussion" modal
-  const closeAddModal = () => {
-    setAddModalOpen(false);
-    setNewDiscussionText('');
-    setSelectedFromList(null);
-  };
-
-  // Handle new discussion submission
-  const handleAddDiscussion = () => {
-    if (!selectedFromList) {
-      alert('Please select a theme.');
-      return;
-    }
-
-    if (newDiscussionText.trim() === '') {
-      alert('Discussion text cannot be empty.');
-      return;
-    }
-
-    // Call the addDiscussion service
-    addDiscussion(selectedFromList, newDiscussionText)
-      .then(() => {
-        alert('Discussion added successfully.');
-        closeAddModal(); // Close the modal after submission
-      })
-      .catch((error) => {
-        console.error('Error adding discussion:', error);
-        alert('Failed to add discussion.');
-      });
-  };
-
-  
-  // ****ZA EDIT DISKUSIJA KORISNIKA KOJI JE TRENUTNO LOGOVAN 
-    // dobavicemo sve diskusije trenutno logovanog korisnika (iz sessionStorage-a)
- const handleFetchUserDiscussions = async() => {
-    const username = sessionStorage.getItem("user_name");
-
-    if (!username) {
-      alert('Nema podataka o korisniku u sesiji.');
-      return;
-    }
-
-    fetchDiscussionsOfUser(username) // Assuming fetchDiscussions can handle fetching by username
-      .then((response) => {
-        if (response) {
-          console.log(response.discussions); // You might also want to log response.data here
-          setUserDiscussions(response.discussions); // This should be an array
-          setUserDiscussionsVisible(!isUserDiscussionsVisible); // Toggle visibility
-          setDiscussionsVisible(false);
-        } else {
-          console.error('Nema diskusija u odgovoru');
-        }
-      })
-      .catch((error) => {
-        console.error('Greška pri dohvatiti diskusije korisnika:', error);
-        alert('Došlo je do greške pri učitavanju diskusija korisnika.');
-      });
-  };
-
-
-  useEffect(() => {
-    if (editDiscussion) {
-      setEditDiscussionModalOpen(true); // Open modal only when editUser is updated
-    }
-  }, [editDiscussion]);
-  
-  
-  const handleInputDiscussionChange = (e) => {
-    const { name, value } = e.target;
-  setEditDiscussion((prevDiscussion) => ({ ...prevDiscussion, [name]: value }));
-  }
-
-  const handleEditDiscussion = () => {
-   // salje izmenjeni objekat bekendu 
-   if (!editDiscussion) {
-    alert('No discussion to update.');
-    return;
-  }
-  console.log("SALJEM NA IZMENU OVU DISKUSIJU  ID:", editDiscussion.id)
-  console.log(editDiscussion)
-  updateDiscussion(editDiscussion.id, editDiscussion)
-    .then(() => {
-      alert('Discussion updated successfully.');
-      setEditDiscussionModalOpen(false); // Close modal
-      setEditDiscussion(null); // Reset state
-      handleFetchUserDiscussions(); // Refresh user's discussions
-    })
-    .catch((error) => {
-      console.error('Error updating discussion:', error);
-      alert('Failed to update discussion.');
-    });
-};
-
-
-  const closeEditDiscussionModal = () => {
-    setEditDiscussionModalOpen(false);
-    setEditDiscussion(null);
-  };
-
-  // prosledimo diskusiju kao parametr, klikom na edit button se salje kao parametar u ovu funkciju
-  const handleUserDiscussionEdit = (discussion) =>{
-    console.log(discussion.id)
-    console.log(discussion.content)
-    
-    // poziva se na klik pen-a
-    // treba da dobavimo diskusiju po prosledjenom id-u
-    // u modalu za edit prosledimo dobavljenu diskusiju
-    getDiscussionById(discussion.id)
-    .then((response) => {
-      console.log(response.discussion);
-      setEditDiscussion(response.discussion); // Set the discussion data in state
-    })
-    .catch((error) => {
-      console.error('Error fetching discussion:', error);
-      alert('Failed to fetch the discussion for editing.');
-    });
-    // klikom na save handleEditDiscussion 
-    
-  }
-
-//******************BRISANJE DISKUSIJE */
-// Function to handle discussion deletion
-const handleDeleteDiscussion = (discussionId) => {
-  console.log(`Deleting discussion with ID: ${discussionId}`);
-  // Assuming you have a service function `deleteDiscussion`
-  deleteDiscussion(discussionId)
-    .then(() => {
-      alert('Discussion deleted successfully.');
-      handleFetchUserDiscussions(); // Refresh user's discussions
-    })
-    .catch((error) => {
-      console.error('Error deleting discussion:', error);
-      alert('Failed to delete discussion.');
-    });
-};
-
-///////////////////////////////////
-  // novi kod , ZA EDITOVANJE KORISNIKA 
-   // za edit korisnika : 
+   // KOD ZA EDITOVANJE KORISNIKA 
    const openEditModal = async () => {
     const userString = sessionStorage.getItem("user_name");
     console.log(userString);
@@ -223,11 +50,10 @@ const handleDeleteDiscussion = (discussionId) => {
     }
   };
   
-  // Open the modal after editUser is updated
   // ovde editUser ima vrednost
   useEffect(() => {
     if (editUser) {
-      setEditModalOpen(true); // Open modal only when editUser is updated
+      setEditModalOpen(true); 
     }
   }, [editUser]);
   
@@ -244,111 +70,32 @@ const handleDeleteDiscussion = (discussionId) => {
     }
 
     const username = sessionStorage.getItem("userName");
-    console.log("AAAAAAA")
-    console.log(editUser.name)
-    console.log(editUser.id)
+
     if (!username) {
         alert('Korisničko ime nije pronađeno.');
         return;
     }
-      // poziva se funkcija iz servisa react-a
-      // koja salje id i editovanogUsera
+  
     const userString = sessionStorage.getItem("user_name");
     updateUser(userString, editUser)
         .then((updatedUser) => {
             console.log('Korisnik uspešno ažuriran:', updatedUser);
             alert('Korisnik uspešno ažuriran.');
-            closeEditModal(); // Close the modal after successful update
+            closeEditModal(); 
         })
         .catch((error) => {
             console.error('Greška pri ažuriranju korisnika:', error);
             alert('Došlo je do greške pri ažuriranju korisnika.');
         });
-};
-
+  };
 
   const closeEditModal = () => {
     setEditModalOpen(false);
     setEditUser(null);
   };
-////////
-
-  // Rukovanje klikom na temu u levom sidebaru
-  const handleSelectTheme = (theme) => {
-    console.log(theme);
-    setSelectedTheme(theme);
-    // Dohvatanje diskusija za selektovanu temu
-    handleFetchDiscussions(theme.id);
-    setUserDiscussionsVisible(false);
-    setDiscussionsVisible(true);
-  };
-
-  // Dohvatanje diskusija za selektovanu temu
-  const handleFetchDiscussions = (themeId) => {
-    fetchDiscussions(themeId) // Pretpostavljam da fetchDiscussions uzima id teme i vraća diskusije
-      .then((response) => {
-        setDiscussions(response.data); // Stavljanje diskusija u state
-      })
-      .catch((error) => {
-        console.error('Greška pri dohvatiti diskusije:', error);
-      });
-  };
-
-  // Rukovanje unosom diskusije
-  const handlePublishDiscussion = () => {
-    if (!selectedTheme) {
-      alert('Molimo izaberite temu pre objave diskusije.');
-      return;
-    }
-    if (discussionText.trim() === '') {
-      alert('Tekst diskusije ne može biti prazan.');
-      return;
-    }
-
-    // Simulacija dodavanja diskusije u selektovanu temu
-    console.log(`Diskusija za temu "${selectedTheme.theme_name}":`, discussionText);
-    // diskusija treba biti za temu sa odredjenim id-em
-    addDiscussion(selectedTheme, discussionText)
-
-    // Resetovanje unosa nakon objave
-    setDiscussionText('');
-  };
-
-  // Rukovanje promenom u polju za pretragu - ispravljen exception
-  const handleSearchChange = (e) => {
-    const value = e.target.value; 
-    setSearchTerm(value);
-  
-    if (!value.trim()) {
-      setFilteredThemes(themes);
-      return;
-    }
-    //admin mijenja, dodaje i brise teme, koristii socket da obavijestimo korisnika o promjeni
-    //ovako samo jednom trazimo listu tema i ne azuriramo je nikad - kad se implementira to kod admina
-    const filtered = themes.filter(
-      (theme) =>
-        theme &&
-        theme.theme_name &&
-        theme.theme_name.toLowerCase().includes(value.toLowerCase()) 
-    );
-    console.log(filtered);
-
-    setFilteredThemes(filtered);
-  };
 
   useEffect(() => {
-    fetchThemes()
-      .then((response) => {
-        const sortedThemes = response.data.sort(
-          (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
-        );
-        console.log(sortedThemes)
-        setThemes(sortedThemes);
-        setFilteredThemes(sortedThemes);
-      })
-      .catch((error) => console.error('Error fetching themes:', error));
-
-    // Ovde možete dodati socket logiku, ako je potrebno
+    // Ovde možemo dodati socket logiku, ako je potrebno
     return () => {
       socket.off("serverReaction");
       socket.off("mention_notification");
@@ -357,170 +104,23 @@ const handleDeleteDiscussion = (discussionId) => {
 
   return (
     <div className="d-flex flex-column w-100" style={{ height: '100vh' }}>
-    {/* Logout Button */}
-    {/* <button onClick={handleLogout} className="logout-btn">
-      Logout
-    </button> */}
-  
+
      {/* Centrirana Discussions komponenta sa 70% visine */}
      <div className="d-flex justify-content-center align-items-start" style={{ height: '70vh' }}>
-      {/* Prosleđivanje samo userId kao prop u Discussions */}
       {userId && <Discussions userId={userId} />}
-    </div>
-
-
-     <div className="sidebar-left">
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Pretraži teme..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-        </div>
-        <div className="theme-list">
-          {filteredThemes.map((theme) => (
-            <div
-              key={theme.id}
-              className={`theme-item ${
-                selectedTheme && selectedTheme.id === theme.id ? 'selected' : ''
-              }`}
-              onClick={() => handleSelectTheme(theme)}
-            >
-              <h4>{theme.id}</h4>
-              <p>{theme.theme_name}</p>
-              {/* <p>{theme.date_time}</p> */}
-            </div>
-          ))}
-        </div>
-      </div>
-      
-      
-      
-      {isEditDiscussionModalOpen && (
-      <div className="edit-modal">
-        <div className="modal-content">
-            <h4>Uredi diskusiju</h4>
-            <form>
-                <label>
-                    Naslov:
-                    <input
-                        type="text"
-                        name="title"
-                        value={editDiscussion?.title || ''}
-                        onChange={handleInputDiscussionChange}
-                    />
-                </label>
-                <label>
-                    Sadržaj:
-                    <textarea
-                        name="content"
-                        value={editDiscussion?.content || ''}
-                        onChange={handleInputDiscussionChange}
-                        rows="5"
-                    />
-                </label>
-            </form>
-            <button onClick={handleEditDiscussion}>Sačuvaj</button>
-            <button onClick={closeEditDiscussionModal}>Zatvori</button>
-        </div>
-      </div>
-      )}
+     </div>
 
     <div className="d-flex flex-column w-100" style={{ height: '100vh' }}>
-      {/* Right Sidebar */}
+      {/* Desni Sidebar */}
       <div className="top-right-controls">
         <div className="in_line">
-        <button
-          className="btn btn-info"
-          onClick={openAddModal}
-          style={{ margin: '1rem' }}
-        >
-        Add
-        </button>
         <button className="btn btn-info" onClick={openEditModal} style={{ margin: '1rem' }} >Edit</button>
-        {/* <FontAwesomeIcon
-          icon={faUserEdit}
-          className="edit-icon"
-          onClick={openEditModal}
-        /> */}
-      <button onClick={handleLogout} className="btn btn-secondary">
-      Logout
-    </button>
+        <button onClick={handleLogout} className="btn btn-secondary"> Logout </button>
         </div>
-
-        {/* Add Discussion Modal */}
-        {isAddModalOpen && (
-          <div className="edit-modal">
-           <div className="modal-content">
-              <h4>Add Discussion</h4>
-              <form>
-                <label htmlFor="themeDropdown">Select Theme:</label>
-                <select
-                id="themeDropdown"
-                className="form-select"
-                value={selectedFromList ? selectedFromList.id : ''}
-                onChange={(e) =>
-                  setSelectedFromList(
-                    themes.find((theme) => theme.id === parseInt(e.target.value))
-                  )
-                }
-              >
-                <option value="" disabled>
-                  Choose a theme...
-                </option>
-                {themes.map((theme) => (
-                  <option key={theme.id} value={theme.id}>
-                    {theme.theme_name}
-                  </option>
-                ))}
-              </select>
-
-              <label htmlFor="discussionText" className="mt-3">
-                Discussion Text:
-              </label>
-              <textarea
-                id="discussionText"
-                className="form-control"
-                rows="5"
-                value={newDiscussionText}
-                onChange={(e) => setNewDiscussionText(e.target.value)}
-                placeholder="Enter your discussion here..."
-              ></textarea>
-            </form>
-
-            <div className="mt-3">
-              <button
-                className="btn btn-info me-2"
-                onClick={handleAddDiscussion}
-              >
-                Add
-              </button>
-              <button className="btn btn-secondary" onClick={closeAddModal}>
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-       {/* <div className="header">
-        <FontAwesomeIcon
-          icon={faUserEdit}
-          className="edit-icon"
-          onClick={openEditModal}
-        />
-        <FontAwesomeIcon
-          icon={faList}
-          className="fetch-icon"
-          onClick={handleFetchUserDiscussions}
-        />
-       </div> */}
+      </div>
     </div>
 
-    </div>
-
-
-      {/* Modal for Editing User */}
+      {/* Modal za edit - User */}
       {isEditModalOpen && (
         <div className="edit-modal">
           <div className="modal-content">
@@ -586,38 +186,8 @@ const handleDeleteDiscussion = (discussionId) => {
           </div>
         </div>
       )}
-
-
-      {/* Discussions List */}
-      <div className="discussion-list">
-        {isUserDiscussionsVisible && (
-          <div className="discussions-list">
-            <h5></h5>
-            
-            {userDiscussions.map((discussion) => (
-              <div key={discussion.id} className="discussion-item">
-                <h6>{discussion.title}</h6>
-                <p>{discussion.content}</p>
-                <p>{discussion.datetime}</p>
-                <FontAwesomeIcon
-                  icon={faPen}
-                  className="edit-icon1"
-                  onClick={() => handleUserDiscussionEdit(discussion)}
-                />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className="delete-icon"
-                  onClick={() => handleDeleteDiscussion(discussion.id)}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
     </div>
   );
 };
-
-
 
 export default User;
