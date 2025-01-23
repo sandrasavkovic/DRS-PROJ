c937a1d92cfa7e0cd185bab558e1d185bdf8d208
duskamuncan <duskamuncan113@gmail.com>
2025-01-23 11:36:56 +0100

    izmene za railway bazu7

--- flask-server/db.py
+++ flask-server/db.py
@@ -11,7 +11,7 @@ import os
 
 def get_db_connection():
     # Uzimanje vrednosti iz environment varijabli
-    host = os.getenv("MYSQLHOST", "junction.proxy.rlwy.net")  # Default je 'localhost' ako nije postavljeno izmeni ako treba za railway
+    host = os.getenv("MYSQLHOST", "mysql-lrr3-production.up.railway.app")  # Default je 'localhost' ako nije postavljeno izmeni ako treba za railway
     user = os.getenv("MYSQLUSER", "root")
     password = os.getenv("MYSQLPASSWORD","duska")
     database = os.getenv("MYSQLDATABASE", "discussion_app")
